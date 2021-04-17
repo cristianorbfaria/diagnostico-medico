17f88b9055d50b0e9e6c5e818f16dec849f7b9cd
Cristiano Ribeiro <cristianorbfaria@gmail.com>
2021-04-17 16:09:18 -0300

    Ajustado a metodo 'informarSintomas' para informar o codigo do sintoma

--- src/main.java
+++ src/main.java
@@ -1,5 +1,7 @@
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.List;
 
@@ -18,9 +20,9 @@ public class main {
 	        
 	        switch (opcao) {
 	        	case 1:	
-	        		List<String> sintomas = informarSintomas();
-	            	System.out.println("Sintomas informados: " + diagnostico(sintomas));
-	            	System.out.println("Deseja abrir menu principal? [Sn]");
+	        		List<Integer> sintomasSelecionados = informarSintomas();
+	            	System.out.println("Sintomas informados: " + diagnostico(sintomasSelecionados));
+	            	System.out.println("Deseja voltar ao menu principal? [Sn]");
 	            	String resposta = "s";
 	            	resposta = sc.next();
 	            	if (resposta.equalsIgnoreCase("n")) {
@@ -50,93 +52,170 @@ public class main {
         System.out.println(" ********************************************");	
 	}
 	
-	private static String diagnostico (List<String> sintomas) {
-		String resultadoFinal = sintomas.toString();
-		String[] sintomasCodiv19 = {
-				"febre", 
-				"tosse", 
-				"cansaco", 
-				"calafrios", 
-				"falta_ar", 
-				"dificuldade_respirar", 
-				"fadiga", 
-				"dores_musculares_corporais", 
-				"dor_cabeça", 
-				"perdade_sabor_cheiro", 
-				"dor_garganta", 
-				"congestao",
-				"nariz_escorrendo", 
-				"nausea",
-				"vomito", 
-				"diarreia"};
+	private static String diagnostico (List<Integer> sintomasSelecionados) {
+		String resultadoFinal = sintomasSelecionados.toString();
 		
-		String[] sintomasDengue  = {
-				"dores_musculos", 
-				"dores_costas", 
-				"dores_ossos", 
-				"febre", 
-				"fadiga", 
-				"mal_estar", 
-				"vômito", 
-				"perda_apetite", 
-				"tremor", 
-				"suor", 
-				"dor_cabeça", 
-				"manchas_avermelhadas_nausea", 
-				"dor_corpo", 
-				"dor_fundo_olhos", 
-				"febre", 
-				"dor_abdominal"};
+		String[] provaveisDoencas = null;
+		int[] covid19 = getSintomasPorDoenca("covid-19");
 		
-		String[] sintomasGripe   = {
-				"febre",
-				"clafrios",
-				"dor_cabeca",
-				"tosse",
-				"espirros",
-				"nariz_escorrendo",
-				"dor_garganta",
-				"dor_muscular",
-				"perda_apetite",
-				"cansaco",
-				"falta_ar",
-				"fraqueza_muscular",};
+		for (int i=0; i < covid19.length; i++) {
+			for (int k=0; k < sintomasSelecionados.size(); k++) {
+				if (covid19[i] == sintomasSelecionados.get(k)) {
+				}
+			}
+		}
 		
 		return resultadoFinal;
 	}
 	
-	private static List<String> informarSintomas () {
-    	List<String> sintomas = new ArrayList<String>();
+	private static List<Integer> informarSintomas () {
+    	List<Integer> sintomasSelecionados = new ArrayList<Integer>();
     	
     	Scanner sc = new Scanner(System.in);
-    	int i = 1;
-    	String resposta = "s";
+    	int codigoSintoma = 0;
+    	int i = 0;
     	
     	System.out.println("//////////////////////////////////////////////////////////////////////////");
-    	System.out.println("Para um melhor diagnóstico, será necessário informar pelo menos 3 sintomas!");
+    	System.out.println("Para um melhor diagnóstico, será necessário informar pelo menos 3 sintomas!\n");
+    	
+    	List<String> sintomas = getSintomas();
+    	
+    	// Imprimir sintomas 
+    	for(int k = 1; k < sintomas.size(); k++) {
+    		System.out.println(k + " - " + sintomas.get(k));
+    	}
     	
     	do {
-    		System.out.println("Por favor informe o "+i+"º sintoma:");
-    	    sintomas.add(sc.next());
-    	    
-    	    i++;
-    	    if(i > 3) {
+    		System.out.println("Por favor informe o código do sintoma:");
+    		try {
+    			codigoSintoma = sc.nextInt();
+        		sintomasSelecionados.add(codigoSintoma);
+        	    i++;
+    		} catch (InputMismatchException e) {
+                sc.nextLine();
+                System.out.println("Código inválido! Tente novamente");
+                i--;
+            }
+    		
+    	    if(i > 2) {
     	    	System.out.println("Deseja informar mais um sintoma? [Sn]");
-    	    	resposta = "";
-	        	resposta = sc.next();
-	        	if (resposta.equalsIgnoreCase("s")) {
-        			System.out.println("Por favor informe o "+i+"º sintoma:");
-        	    	sintomas.add(sc.next());
-        	    	i++;
-        	    	System.out.println("Deseja informa mais um sintoma? [Sn]");
-        	    	resposta = "";
-        	    	resposta = sc.next();
+	        	String resposta = sc.next();
+	        	if (resposta.equalsIgnoreCase("n")) {
+	        		break;
 	        	}
     	    }
-    	} while (resposta.equalsIgnoreCase("s"));
+    	} while (true);
     	System.out.println("//////////////////////////////////////////////////////////////////////////");
+    	
+		return sintomasSelecionados;
+	}
+	
+	private static List<String> getSintomas () {
+    	
+		List<String> sintomas = new ArrayList<String>();
+		
+		// O index definie ID de cada sintoma
+    	sintomas.addAll(Arrays.asList(
+    			"",
+    			"Febre",
+    			"Tosse",
+    			"Cansaço",
+    			"Calafrios",
+    			"Falta de ar",
+    			"Dificuldade para respirar",
+    			"Fadiga",
+    			"Dor muscular",
+    			"Dor de cabeça",
+    			"Dor de garganta",
+    			"Perda de sabor",
+    			"Perda de cheiro",
+    			"Congestão",
+    			"Vômito",
+    			"Diarréia",
+    			"Dor nas costas",
+    			"Dor nos ossos",
+    			"Dor no corpo",
+    			"Dor no fundo dos olhos",
+    			"Dor abdominal",
+    			"Mal-estar",
+    			"Perda de apetite",
+    			"Tremor",
+    			"Suor",
+    			"Manchas avermelhadas",
+    			"Náusea",
+    			"Espirros",
+    			"Nariz escorrendo",
+    			"Fraqueza muscular"));
     	
 		return sintomas;
 	}
-
+	
+	private static int[] getSintomasPorDoenca (String doenca) {
+		
+		// Sintomas definidos conforme ID
+		// Associação do ID com nome do sintoma encontra-se no metodo "informarSintomas" -> arrayList "sintomas"
+		// O index define ID do sintoma
+		int[] sintomasCodiv19 = {
+				1, 
+				2, 
+				3, 
+				4, 
+				5, 
+				6, 
+				7, 
+				8, 
+				9,
+				10,
+				11,
+				12,
+				13, 
+				26,
+				14, 
+				15};
+		
+		int[] sintomasDengue  = {
+				8, 
+				16, 
+				17, 
+				1, 
+				7, 
+				21, 
+				14, 
+				22, 
+				23, 
+				24, 
+				9, 
+				25,
+				26, 
+				18, 
+				19,  
+				20};
+		
+		int[] sintomasGripe   = {
+				1,
+				4,
+				9,
+				2,
+				27,
+				28,
+				10,
+				8,
+				22,
+				3,
+				5,
+				29};
+		
+		switch (doenca) {
+		case "covid-19":
+			return sintomasCodiv19;
+		case "dengue":
+			return sintomasDengue;
+		case "gripe":
+			return sintomasGripe;
+		default:
+			return null;
+		
+		}
+		
+	}
 }
