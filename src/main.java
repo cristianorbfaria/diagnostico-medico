6a84ae72f9ee00498ad3b961b6c903904703e641
Cristiano Ribeiro <cristianorbfaria@gmail.com>
2021-04-24 16:53:29 -0300

    Adicionado comentários em todo o código; \nAdicionado uma função para validar se o usuário digitou o mesmo sintoma anteriormente; \nCriado validção se o usuário digitou algum valor diferente de número do metodo 'informarSintomas'; \nCriado validação se o usuário digitou um valor diferente de n ou s quando sistema solicita se deseja retornar para o menu principal

--- src/main.java
+++ src/main.java
@@ -1,4 +1,3 @@
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.InputMismatchException;
@@ -7,50 +6,63 @@ import java.util.List;
 
 public class main {
 
+	// Metodo principal para iniciar o sistema.
 	public static void main(String[] args) {
-		
-		int opcao = 0;
-		
+		// Variavel utilizada para receber entrada do usuário, podemos iniciar um diagnóstico ou encerrar o sistema.
+		int opcao = -1;
+		// Loop para que o usuário possa navegar no menu.
 		do {
 			// Imprimir menu
 			menu();
 	        System.out.print("\nDigite aqui sua opção:");
+	        // Inicializando objeto Scanner, utilizado para receber entrada do usuário.
     		Scanner sc = new Scanner(System.in);
-	        opcao = Integer.parseInt(sc.nextLine());
-	        
+    		// Aguardando usuário digitar uma opção de acordo com o menu.
+    		try {
+    			opcao = sc.nextInt();
+    		} catch (InputMismatchException e) {
+                System.out.println("\nOpção inválida! Tente novamente!");
+            }
+	        // Switch case para executar codigo de acordo com a opção escolhida pelo usuário.
 	        switch (opcao) {
+	        	// Case para solicitar os sintomas ao usuário e realizar o pré-diagnóstico.
 	        	case 1:	
+	        		// Lista para que seja preenchida com o codigo de cada sintoma que o usuário digitar.
 	        		List<Integer> sintomasSelecionados = informarSintomas();
 	        		System.out.println("\nSintomas informados: ");
+	        		// Lista que recebe todos os sintomas reconhecidos pelo sistema.
 	        		List<String> sintomas = getSintomas();
+	        		// Loop para imprimir os sintomas informados pelo usuário.
 	        		for (int i=0; i < sintomasSelecionados.size(); i++) {
 		            	System.out.println("- "+sintomas.get(sintomasSelecionados.get(i))); 	        			
 	        		}
 	            	System.out.println("\n***************** Diagnóstico *****************\n");
+	            	// Neste momento será repassado como parâmetro para o metodo 'diagnostico' todos os sintomas informados pelo usuário e imprime o resultado.
 	            	System.out.println(diagnostico(sintomasSelecionados));
 	            	System.out.println("\n***********************************************");
-	            	System.out.println("\nDeseja voltar ao menu principal? [S/N]");
-	            	String resposta = "s";
-	            	resposta = sc.next();
-	            	if (resposta.equalsIgnoreCase("n")) {
-	                    System.out.println("\n****** FIM DO PROGRAMA ******");
-	            		System.exit(0);
-	            	}
+	            	String resposta; 
+	            	do {
+			            System.out.println("\nDeseja voltar ao menu principal? [S/N]");
+			            // Aguarda usuário digitar 's' (SIM) para voltar para menu principal, ou 'n' (NÂO) para encerrar o sistema.
+			            resposta = sc.next();
+		            	// Função que valida se o usuário digito 'n' (NÂO) para encerrar o sistema.
+		            	if (resposta.equalsIgnoreCase("n")) {
+		                    System.out.println("\n****** FIM DO PROGRAMA ******");
+		            		System.exit(0);
+		            	}
+	            	} while((!resposta.equalsIgnoreCase("s")) && (!resposta.equalsIgnoreCase("n")));
 	            	break;
+	            // Case para encerrar o sistema caso o usuário digite 0.
                 case 0:
                     System.out.println("\n****** FIM DO PROGRAMA ******");
                     System.exit(0);
-                default:
-                    System.out.println("\nOpção Inválida!");
-                    break;
             }
-
-		} while(opcao != 0);
-		
+		} while(opcao != 0);	
         System.out.println("\n****** FIM DO PROGRAMA ******");
+        // Encerrar o sistema.
         System.exit(0);
 	}
-	
+	// Metodo para imprimir o menu principal.
 	private static void menu () {
         System.out.println(" ****** Sistema Diagnóstico Médico ******");
         System.out.println(" * Escolha uma das opções abaixo ********");
@@ -58,39 +70,42 @@ public class main {
         System.out.println(" * 0 -> Sair ****************************");
         System.out.println(" ****************************************");	
 	}
-	
+	// Metodo para realizar diagnostico de acordo com os sintomas passado como parâmetro.
 	private static String diagnostico (List<Integer> sintomasSelecionados) {
+		// Variaveis para definir pontuação das doenças que mais teve sintomas informados pelo usuário.
 		int pontuacaoCovid19;
 		int pontuacaoGripe;
-		int pontuacaoDengue;		
-		
+		int pontuacaoDengue;
+		// As variaveis abaixo são para receber a pontuação
 		pontuacaoCovid19 = getPontuacaoCovid19(sintomasSelecionados);
 		pontuacaoGripe = getPontuacaoGripe(sintomasSelecionados);
 		pontuacaoDengue = getPontuacaoDengue(sintomasSelecionados);
-		
+		// Função para analisar se a pontuação da doença Covid-19 e maior que as demais doenças para determinar a doença.
 		if (pontuacaoCovid19 > pontuacaoGripe &&
 			pontuacaoCovid19 > pontuacaoDengue) {
 			return ">>>>>>>>>>>>>>>>>> Covid-19 <<<<<<<<<<<<<<<<<<<";
 		}
-		
+		// Função para analisar se a pontuação da doença Gripe e maior que as demais doenças para determinar a doença.
 		if (pontuacaoGripe > pontuacaoCovid19 &&
 			pontuacaoGripe > pontuacaoDengue) {
 			return ">>>>>>>>>>>>>>>>>>>> Gripe <<<<<<<<<<<<<<<<<<<<";
 		}
-		
+		// Função para analisar se a pontuação da doença Dengue e maior que as demais doenças para determinar a doença.
 		if (pontuacaoDengue > pontuacaoCovid19 &&
 			pontuacaoDengue > pontuacaoGripe) { 
 			return ">>>>>>>>>>>>>>>>>>> Dengue <<<<<<<<<<<<<<<<<<<<";
 		}
-		
+		// Caso não consiga determinar a doença, retorna como diagnóstico impreciso.
 		return "> Impreciso, por favor informe mais sintomas. <";
 	}
-
+	// Metodo para analisar se os sintomas informados pelo usuário constam na doença Covid-19.
+	// Cada sintoma que consta na doença, a variavel pontuacao soma mais 1.
 	private static int getPontuacaoCovid19 (List<Integer> sintomasSelecionados) {
+		// Variavel para pontuação.
 		int pontuacao = 0;
-			
+		// Variavel que recebe codigo de cada sintoma da doença Covid-19.
 		int[] covid19 = getSintomasPorDoenca("covid-19");
-		
+		// Loop para analisar se os sintomas informados constam na doença Covid-19.
 		for (int i=0; i < covid19.length; i++) {
 			for (int k=0; k < sintomasSelecionados.size(); k++) {
 				if (covid19[i] == sintomasSelecionados.get(k)) {
@@ -98,15 +113,15 @@ public class main {
 				}
 			}
 		}
-		
 		return pontuacao;		
 	}
-	
+	// Metodo para analisar se os sintomas informados pelo usuário constam na doença Gripe.
+	// Cada sintoma que consta na doença, a variavel pontuacao soma mais 1.
 	private static int getPontuacaoGripe(List<Integer> sintomasSelecionados) {
 		int pontuacao = 0;
-		
+		// Variavel que recebe codigo de cada sintoma da doença Gripe.
 		int[] gripe = getSintomasPorDoenca("gripe");
-		
+		// Loop para analisar se os sintomas informados constam na doença Gripe.
 		for (int i=0; i < gripe.length; i++) {
 			for (int k=0; k < sintomasSelecionados.size(); k++) {
 				if (gripe[i] == sintomasSelecionados.get(k)) {
@@ -114,15 +129,15 @@ public class main {
 				}
 			}
 		}
-		
 		return pontuacao;
 	}
-	
+	// Metodo para analisar se os sintomas informados pelo usuário constam na doença Dengue e retonar a pontuação.
+	// Cada sintoma que consta na doença, a variavel pontuacao soma mais 1.
 	private static int getPontuacaoDengue(List<Integer> sintomasSelecionados) {
 		int pontuacao = 0;
-		
+		// Variavel que recebe codigo de cada sintoma da doença Dengue.
 		int[] dengue = getSintomasPorDoenca("dengue");
-		
+		// Loop para analisar se os sintomas informados constam na doença Dengue.
 		for (int i=0; i < dengue.length; i++) {
 			for (int k=0; k < sintomasSelecionados.size(); k++) {
 				if (dengue[i] == sintomasSelecionados.get(k)) {
@@ -130,57 +145,75 @@ public class main {
 				}
 			}
 		}
-		
 		return pontuacao;
 	}
-	
+	// Metodo para solicitar ao usuário os sintomas.
 	private static List<Integer> informarSintomas () {
+		// Lista para receber codigo de cada sintoma que o usuário informar.
     	List<Integer> sintomasSelecionados = new ArrayList<Integer>();
-    	
     	Scanner sc = new Scanner(System.in);
     	int codigoSintoma = 0;
     	int i = 0;
-    	
     	System.out.println("//////////////////////////////////////////////////////////////////////////");
     	System.out.println("\nPara um melhor diagnóstico, será necessário informar pelo menos 3 sintomas!\n");
-    	
+    	// Lista para receber todos os sintomas reconhecidos pelo sistema.
     	List<String> sintomas = getSintomas();
-    	
-    	// Imprimir sintomas 
+    	// Loop para imprimir sintomas.
     	for(int k = 1; k < sintomas.size(); k++) {
     		System.out.println(k + " - " + sintomas.get(k));
     	}
-    	
+    	// Loop para receber todos os sintomas do usuário
     	do {
-    		System.out.println("\nPor favor informe o código do sintoma:");
+    		System.out.println("\nPor favor informe o código do sintoma: ("+(i+1)+")");
+    		// Try catch para tratar exceção caso o usuário digitar um valor diferente de inteiro.
     		try {
+    			// Variavel para receber codigo do sintoma.
     			codigoSintoma = sc.nextInt();
+    			// Guarda codigo do sintoma na lista 'sintomasSelecionados'.
         		sintomasSelecionados.add(codigoSintoma);
-        	    i++;
+        		i++;
+       		// Caso digite algum valor diferente de número, gera uma exceção.
     		} catch (InputMismatchException e) {
                 sc.nextLine();
-                System.out.println("\nCódigo inválido! Tente novamente");
-                i--;
+                System.out.println("\nCódigo inválido! Tente novamente!");
             }
-    		
-    	    if(i > 2) {
-    	    	System.out.println("\nDeseja informar mais um sintoma? [S/N]");
-	        	String resposta = sc.next();
-	        	if (resposta.equalsIgnoreCase("n")) {
-	        		break;
-	        	}
+    		// Valida se o codigo do sintoma e maior que 29, pois existe somente 29 sintomas.
+    	    if (codigoSintoma > 29) {
+    	    	System.out.println("\nCódigo inválido! Tente novamente!");
+    	    	i--;
+    	        sintomasSelecionados.remove(i);
+    	    }
+    	    // Loop para validar se o codigo do sintoma já foi informado anteriormente.
+    	    for(int j=0;j < (sintomasSelecionados.size()-1);j++) {
+    	    	if(codigoSintoma == sintomasSelecionados.get(j)) { 
+        	    	System.out.println("\nSintoma já informado! Digite novamente!");
+        	    	i--;
+        	        sintomasSelecionados.remove(i);
+    	    	}
+    	    }
+    		// Função para solicitar o usuário se deseja informar mais um sintoma, depois que já tiver informado 3 sitnomas.
+    	    if(i > 2) {     	
+            	String resposta; 
+            	do {
+		            System.out.println("\nDeseja informar mais um sintoma? [S/N]");
+		            // Aguarda usuário digitar 's' (SIM) para informar um novo sintoma, ou 'n' (NÂO) para realizar o diagnóstico.
+		            resposta = sc.next();
+            	} while((!resposta.equalsIgnoreCase("s")) && (!resposta.equalsIgnoreCase("n")));
+            	// função que valida se o usuário digito 'n' (não).
+            	if (resposta.equalsIgnoreCase("n")) {
+                    break;
+            	}
     	    }
     	} while (true);
     	System.out.println("//////////////////////////////////////////////////////////////////////////");
-    	
 		return sintomasSelecionados;
 	}
-	
+	// Metodo para preencher a lista com todos os sintomas baseado nas doenças Covid-19, Dengue e Gripe.
 	private static List<String> getSintomas () {
-    	
+    	// Lista para receber todos os sintomas reconhecidos pelo sistema.
 		List<String> sintomas = new ArrayList<String>();
-		
-		// O index definie ID de cada sintoma
+		// Recebe todos os sintomas baseado nas doenças Covid-19, Dengue e Gripe.
+		// Obs.: O index definie ID de cada sintoma.
     	sintomas.addAll(Arrays.asList(
     			"",
     			"Febre",
@@ -197,7 +230,7 @@ public class main {
     			"Perda de cheiro",
     			"Congestão",
     			"Vômito",
-    			"Diarréia",
+    			"Diarreia",
     			"Dor nas costas",
     			"Dor nos ossos",
     			"Dor no corpo",
@@ -212,15 +245,12 @@ public class main {
     			"Espirros",
     			"Nariz escorrendo",
     			"Fraqueza muscular"));
-    	
 		return sintomas;
 	}
-	
+	// Metodo para retornar os sintomas da doença passada como parâmetro.
 	private static int[] getSintomasPorDoenca (String doenca) {
-		
-		// Sintomas definidos conforme ID
-		// Associação do ID com nome do sintoma encontra-se no metodo "informarSintomas" -> arrayList "sintomas"
-		// O index define ID do sintoma
+		// Sintomas definidos conforme ID (index) preenchidos no metodo "informarSintomas" -> arrayList "sintomas".
+		// ID dos sintomas da doença Covid-19.
 		int[] sintomasCodiv19 = {
 				1, 
 				2, 
@@ -238,7 +268,7 @@ public class main {
 				26,
 				14, 
 				15};
-		
+		// ID dos sintomas da doença Dengue.
 		int[] sintomasDengue  = {
 				8, 
 				16, 
@@ -256,7 +286,7 @@ public class main {
 				18, 
 				19,  
 				20};
-		
+		// ID dos sintomas da doença Gripe.
 		int[] sintomasGripe   = {
 				1,
 				4,
@@ -270,7 +300,7 @@ public class main {
 				3,
 				5,
 				29};
-		
+		// Switch case para retonar os sintomas de acordo com a doença passada como parâmetro, caso não encontre, retorna null.
 		switch (doenca) {
 		case "covid-19":
 			return sintomasCodiv19;
@@ -280,7 +310,6 @@ public class main {
 			return sintomasGripe;
 		default:
 			return null;
-		
 		}
 		
 	}
